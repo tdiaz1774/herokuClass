606fb1c8e85bcc2dc1bc92a8a6e6aecfa063452b
Tomás Diaz Servin <57371220+tdiaz1774@users.noreply.github.com>
2022-04-04 20:18:50 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://www.google.com/url?sa=i&url=http%3A%2F%2Fmemegenerator.net%2Finstance%2F47239540&psig=AOvVaw1QgKUszeoC4ZqsUnuLSrEl&ust=1649207282107000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCLCEvoTe-_YCFQAAAAAdAAAAABAc">
+        //<img src="https://www.google.com/url?sa=i&url=http%3A%2F%2Fmemegenerator.net%2Finstance%2F47239540&psig=AOvVaw1QgKUszeoC4ZqsUnuLSrEl&ust=1649207282107000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCLCEvoTe-_YCFQAAAAAdAAAAABAc">
         <p>
           Peña come home :(
         </p>
